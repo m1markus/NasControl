a4f0a743aa0f74f300275bf2420ad6e225dd581b
user667 <elmundi@gmail.com>
2019-12-23 23:12:11 +0100

    Extract UI logic to its own class

--- src/main/java/ch/m1m/nas/NASControl.java
+++ src/main/java/ch/m1m/nas/NASControl.java
@@ -9,21 +9,8 @@ import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class NASControl {
 
@@ -31,105 +18,22 @@ public class NASControl {
     private static final String OPT_HELP = "help";
     private static final String OPT_VERSION = "version";
 
-    private static Logger log = LoggerFactory.getLogger(NASControl.class);
-
-    private static Config config;
-    private static DriverFreeNAS nasDriver;
-    private static DriverInterface.NasStatus lastNasStatus;
-    private static Platform platform;
-
-    private static int queryIntervalSeconds = 10;
-    private static boolean isDarkMode;
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(NASControl.class);
 
     public static void main(String... args) {
 
         CommandLine clArgs = setupAndParseArgs(args);
 
         List<String> liArgs = Arrays.asList(args);
-        log.info("start {} {} with args: {}", PROGRAM_NAME, Version.getProjectVersion(), liArgs);
+        LOGGER.info("start {} {} with args: {}", PROGRAM_NAME, Version.getProjectVersion(), liArgs);
 
-        config = ConfigUtils.loadConfiguration();
-        nasDriver = new DriverFreeNAS(config);
+        Config config = ConfigUtils.loadConfiguration();
 
-        try {
-            // set application icon
-            //
-            InputStream stream = nasDriver.getClass().getResourceAsStream("/images/nascontrol_icon.png");
-            ImageIcon applIcon = new ImageIcon(ImageIO.read(stream));
-            platform = PlatformGeneric.getInstance();
-            platform.setApplicationIcon(applIcon);
-
-            String systemLookAndFeel = UIManager.getSystemLookAndFeelClassName();
-            log.info("system look and feel: {}", systemLookAndFeel);
-
-            if (systemLookAndFeel == null) {
-                systemLookAndFeel = "javax.swing.plaf.metal.MetalLookAndFeel";
-                log.info("changed look and feel to: {}", systemLookAndFeel);
-            }
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel(systemLookAndFeel);
-            // Turn off metal's use of bold fonts
-            UIManager.put("swing.boldMetal", Boolean.FALSE);
-
-        } catch (Exception e) {
-            log.error("Something went wrong", e);
-            System.exit(1);
-        }
-
-        isDarkMode = platform.isTrayIconModeDark();
-
-        lastNasStatus = DriverInterface.NasStatus.UNKNOWN;
-        String iconName = getTrayIconNameFromStatus(lastNasStatus, isDarkMode);
-
-        createTrayIconMenu(iconName);
-
-        executeStatusLoop(config);
-    }
-
-    public static void executeStatusLoop(Config config) {
-
-        while (true) {
-
-            boolean forceCreateIcon = false;
-
-            String nasVersion = nasDriver.getVersion();
-
-            DriverInterface.NasStatus nasStatus = nasDriver.getStatus();
-            if (nasStatus == DriverInterface.NasStatus.SUCCESS) {
-                queryIntervalSeconds = 30;
-            }
-
-            // update the tray icon only when needed to prevent flickering
-            //
-            if (lastNasStatus != nasStatus) {
-                lastNasStatus = nasStatus;
-                forceCreateIcon = true;
-            }
-
-            // update dark mode flag if changed
-            //
-            boolean isDarkModeNow = platform.isTrayIconModeDark();
-            if (isDarkModeNow != isDarkMode) {
-                isDarkMode = isDarkModeNow;
-                forceCreateIcon = true;
-            }
-
-            String iconName = getTrayIconNameFromStatus(nasStatus, isDarkMode);
-            if (forceCreateIcon) {
-                createTrayIconMenu(iconName);
-            }
-
-            try {
-                TimeUnit.SECONDS.sleep(queryIntervalSeconds);
-            } catch (InterruptedException e) {
-                log.error("sleep() interrupted", e);
-            }
-        }
+        TrayIconUI trayIconUI = new TrayIconUI(config);
+        trayIconUI.executeStatusLoop();
     }
 
     public static CommandLine setupAndParseArgs(String... args) {
-
         // https://commons.apache.org/proper/commons-cli/usage.html
         //
         Options options = new Options();
@@ -149,160 +53,15 @@ public class NASControl {
             }
 
             if (clArgs.hasOption(OPT_VERSION)) {
-                System.out.println(PROGRAM_NAME + " version " + Version.getProjectVersion());
+                LOGGER.info(PROGRAM_NAME + " version " + Version.getProjectVersion());
                 System.exit(0);
             }
 
         } catch (ParseException e) {
-            log.error("parsing command line: {}", e.getMessage());
+            LOGGER.error("parsing command line: {}", e.getMessage());
             System.exit(1);
         }
 
         return clArgs;
-    }
-
-    private static void createTrayIconMenu(String systemTrayIconName) {
-        if (!SystemTray.isSupported()) {
-            log.error("SystemTray is not supported");
-            System.exit(1);
-        }
-        final PopupMenu popup = new PopupMenu();
-
-        // set icons
-        //
-        String imagePath = "/images/" + systemTrayIconName;
-
-        final TrayIcon trayIcon = new TrayIcon(createImage(imagePath, "tray icon"));
-        final SystemTray tray = SystemTray.getSystemTray();
-
-        try {
-            // remove all old icons
-            //
-            TrayIcon[] oldIcons = tray.getTrayIcons();
-            int numOldIcons = oldIcons.length;
-            log.debug("actual {} old icons in the systemTray", numOldIcons);
-            for (int ii = 0; ii < numOldIcons; ii++) {
-                if (oldIcons[ii] != null) {
-                    tray.remove(oldIcons[ii]);
-                }
-            }
-
-            // set the new icon
-            //
-            tray.add(trayIcon);
-
-        } catch (AWTException e) {
-            log.error("TrayIcon could not be added");
-            System.exit(1);
-        }
-
-        // Create a popup menu components
-        //
-        MenuItem sendWOL = new MenuItem("Send WoL");
-        MenuItem sendShutdown = new MenuItem("Send Shutdown");
-        MenuItem openWebUI = new MenuItem("Open WebUI");
-
-        MenuItem exitItem = new MenuItem("Exit");
-
-        //sendWOL.setEnabled(false);
-
-        popup.add(sendWOL);
-        popup.add(openWebUI);
-        popup.addSeparator();
-        popup.add(sendShutdown);
-
-        popup.addSeparator();
-        popup.add(exitItem);
-
-        trayIcon.setPopupMenu(popup);
-
-        trayIcon.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,
-                        "This dialog box is run from System Tray");
-            }
-        });
-
-        sendWOL.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendWakeOnLan();
-            }
-        });
-
-        openWebUI.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                openWebUI();
-            }
-        });
-
-        sendShutdown.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                nasDriver.shutdown();
-            }
-        });
-
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tray.remove(trayIcon);
-                System.exit(0);
-            }
-        });
-    }
-
-    private static void sendWakeOnLan() {
-        List<DatagramPacket> packets = Stream.of(7, 9)
-                .map(port -> WakeOnLanDatagramPacketFactory.newInstance(
-                        config.getMacAddress(), config.getBroadcastAddress(), port))
-                .collect(Collectors.toList());
-
-        try (DatagramSocket socket = new DatagramSocket()) {
-
-            for (DatagramPacket packet : packets) {
-                socket.send(packet);
-            }
-
-        } catch (IOException e) {
-            log.error("Failed to send WoL packet ", e);
-        }
-    }
-
-    private static void openWebUI() {
-        String urlNasWebUI = config.getNasAdminUI();
-        String shellCommand = String.format("open %s", urlNasWebUI);
-
-        log.info("try to spawn sub process: {}", shellCommand);
-        try {
-            Runtime.getRuntime().exec(shellCommand);
-        } catch (IOException e) {
-            log.error("Failed to spawn browser", e);
-        }
-    }
-
-    private static Image createImage(String path, String description) {
-        URL imageURL = NASControl.class.getResource(path);
-        if (imageURL == null) {
-            log.error("Resource not found: {}", path);
-            return null;
-        } else {
-            return (new ImageIcon(imageURL, description)).getImage();
-        }
-    }
-
-    private static String getTrayIconNameFromStatus(DriverInterface.NasStatus status, boolean isDarkMode) {
-        String iconName = null;
-        switch (status) {
-            case UNKNOWN:
-                iconName = "cloud-computing-gray-512x512.png";
-                if (isDarkMode) iconName = "cloud-computing-gray-512x512.png";
-                break;
-            case SUCCESS:
-                iconName = "cloud-computing-black-success-512x512.png";
-                if (isDarkMode) iconName = "cloud-computing-white-success-512x512.png";
-                break;
-            default:
-                iconName = "cloud-computing-black-error-512x512.png";
-                if (isDarkMode) iconName = "cloud-computing-white-error-512x512.png";
-        }
-        return iconName;
     }
 }
---
+++ src/main/java/ch/m1m/nas/TrayIconUI.java
@@ -0,0 +1,245 @@
+package ch.m1m.nas;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TrayIconUI {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrayIconUI.class);
+
+    private final Config config;
+
+    private final DriverFreeNAS nasDriver;
+
+    private Platform platform;
+
+    private DriverInterface.NasStatus lastNasStatus;
+
+    private int queryIntervalSeconds = 10;
+
+    private boolean isDarkMode;
+
+    public TrayIconUI(Config config) {
+        platform = PlatformGeneric.getInstance();
+        isDarkMode = platform.isTrayIconModeDark();
+
+        try {
+
+            // set application icon
+            //
+            final InputStream stream = getClass().getClassLoader().getResourceAsStream("images/nascontrol_icon.png");
+            final BufferedImage image = ImageIO.read(stream);
+            ImageIcon appIcon = new ImageIcon(image);
+            platform.setApplicationIcon(appIcon);
+
+            // Setup look and feel
+            String systemLookAndFeel = UIManager.getSystemLookAndFeelClassName();
+            LOGGER.info("system look and feel: {}", systemLookAndFeel);
+
+            if (systemLookAndFeel == null) {
+                systemLookAndFeel = "javax.swing.plaf.metal.MetalLookAndFeel";
+                LOGGER.info("changed look and feel to: {}", systemLookAndFeel);
+            }
+
+            UIManager.setLookAndFeel(systemLookAndFeel);
+            // Turn off metal's use of bold fonts
+            UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+        } catch (Exception e) {
+            LOGGER.error("Something went wrong", e);
+            System.exit(1);
+        }
+
+        this.config = config;
+        lastNasStatus = DriverInterface.NasStatus.UNKNOWN;
+        nasDriver = new DriverFreeNAS(config);
+
+        String iconName = getTrayIconNameFromStatus(lastNasStatus, isDarkMode);
+
+        createTrayIconMenu(iconName);
+    }
+
+    public void executeStatusLoop() {
+
+        while (true) {
+
+            boolean forceCreateIcon = false;
+
+            String nasVersion = nasDriver.getVersion();
+
+            DriverInterface.NasStatus nasStatus = nasDriver.getStatus();
+            if (nasStatus == DriverInterface.NasStatus.SUCCESS) {
+                queryIntervalSeconds = 30;
+            }
+
+            // update the tray icon only when needed to prevent flickering
+            //
+            if (lastNasStatus != nasStatus) {
+                lastNasStatus = nasStatus;
+                forceCreateIcon = true;
+            }
+
+            // update dark mode flag if changed
+            //
+            boolean isDarkModeNow = platform.isTrayIconModeDark();
+            if (isDarkModeNow != isDarkMode) {
+                isDarkMode = isDarkModeNow;
+                forceCreateIcon = true;
+            }
+
+            String iconName = getTrayIconNameFromStatus(nasStatus, isDarkMode);
+            if (forceCreateIcon) {
+                createTrayIconMenu(iconName);
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(queryIntervalSeconds);
+            } catch (InterruptedException e) {
+                LOGGER.error("sleep() interrupted", e);
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void createTrayIconMenu(String systemTrayIconName) {
+        if (!SystemTray.isSupported()) {
+            LOGGER.error("SystemTray is not supported");
+            System.exit(1);
+        }
+        final PopupMenu popup = new PopupMenu();
+
+        // set icons
+        //
+        String imagePath = "/images/" + systemTrayIconName;
+
+        final TrayIcon trayIcon = new TrayIcon(createImage(imagePath, "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        try {
+            // remove all old icons
+            //
+            TrayIcon[] oldIcons = tray.getTrayIcons();
+            int numOldIcons = oldIcons.length;
+            LOGGER.debug("actual {} old icons in the systemTray", numOldIcons);
+            for (int ii = 0; ii < numOldIcons; ii++) {
+                if (oldIcons[ii] != null) {
+                    tray.remove(oldIcons[ii]);
+                }
+            }
+
+            // set the new icon
+            //
+            tray.add(trayIcon);
+
+        } catch (AWTException e) {
+            LOGGER.error("TrayIcon could not be added");
+            System.exit(1);
+        }
+
+        // Create a popup menu components
+        //
+        MenuItem sendWOL = new MenuItem("Send WoL");
+        MenuItem sendShutdown = new MenuItem("Send Shutdown");
+        MenuItem openWebUI = new MenuItem("Open WebUI");
+
+        MenuItem exitItem = new MenuItem("Exit");
+
+        popup.add(sendWOL);
+        popup.add(openWebUI);
+        popup.addSeparator();
+        popup.add(sendShutdown);
+
+        popup.addSeparator();
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+        trayIcon.addActionListener(e -> JOptionPane.showMessageDialog(null,
+                "This dialog box is run from System Tray"));
+
+        // Setup action listeners on menu items
+        sendWOL.addActionListener(e -> sendWakeOnLan());
+        openWebUI.addActionListener(e -> openWebUI());
+        sendShutdown.addActionListener(e -> nasDriver.shutdown());
+        exitItem.addActionListener(e -> {
+            tray.remove(trayIcon);
+            System.exit(0);
+        });
+    }
+
+    private void sendWakeOnLan() {
+        List<DatagramPacket> packets = Stream.of(7, 9)
+                .map(port -> WakeOnLanDatagramPacketFactory.newInstance(
+                        config.getMacAddress(), config.getBroadcastAddress(), port))
+                .collect(Collectors.toList());
+
+        try (DatagramSocket socket = new DatagramSocket()) {
+
+            for (DatagramPacket packet : packets) {
+                socket.send(packet);
+            }
+
+        } catch (IOException e) {
+            LOGGER.error("Failed to send WoL packet ", e);
+        }
+    }
+
+    private void openWebUI() {
+        String urlNasWebUI = config.getNasAdminUI();
+        String shellCommand = String.format("open %s", urlNasWebUI);
+
+        LOGGER.info("try to spawn sub process: {}", shellCommand);
+        try {
+            Runtime.getRuntime().exec(shellCommand);
+        } catch (IOException e) {
+            LOGGER.error("Failed to spawn browser", e);
+        }
+    }
+
+    private Image createImage(String path, String description) {
+        URL imageURL = getClass().getResource(path);
+        if (imageURL == null) {
+            LOGGER.error("Resource not found: {}", path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
+
+    private String getTrayIconNameFromStatus(DriverInterface.NasStatus status, boolean isDarkMode) {
+        String iconName;
+        switch (status) {
+            case UNKNOWN:
+                iconName = "cloud-computing-gray-512x512.png";
+                if (isDarkMode) {
+                    iconName = "cloud-computing-gray-512x512.png";
+                }
+                break;
+            case SUCCESS:
+                iconName = "cloud-computing-black-success-512x512.png";
+                if (isDarkMode) {
+                    iconName = "cloud-computing-white-success-512x512.png";
+                }
+                break;
+            default:
+                iconName = "cloud-computing-black-error-512x512.png";
+                if (isDarkMode) {
+                    iconName = "cloud-computing-white-error-512x512.png";
+                }
+        }
+        return iconName;
+    }
+}
