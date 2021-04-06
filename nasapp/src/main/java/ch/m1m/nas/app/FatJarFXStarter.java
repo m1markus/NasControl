7dac5502aaf8f83754ad405d6e7c1481ccf678fe
Markus <mmm7@bluewin.ch>
2021-04-06 23:31:31 +0200

    Add JavaFX libs and starter

---
+++ nasapp/src/main/java/ch/m1m/nas/app/FatJarFXStarter.java
@@ -0,0 +1,10 @@
+package ch.m1m.nas.app;
+
+// taken from: https://github.com/danlangford/dice-notation-javafx
+
+public class FatJarFXStarter {
+
+    public static void main(String... args) {
+        NASControl.main(args);
+    }
+}
